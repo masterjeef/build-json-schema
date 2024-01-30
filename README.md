a5dd152225c9488c40467811f3483bc2bcda822a
Jeff Linton <jslinton44@gmail.com>
2024-01-30 16:52:41 -0700

    Init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # build-json-schema
 Converts typescript to a JSON schema
+
+First, install the schema generator:
+`npm install`
+
+Next paste your types into the `types.ts` file:
+
+To generate the JSON schema, run:
+`npm run build-schema`
---
+++ types.ts
@@ -0,0 +1,16 @@
+
+/** Description for IHello */
+export interface IHello {
+    /** description for name */
+    name: string;
+    /** description for id */
+    id: number;
+}
+
+/** Description for IWorld */
+export interface IWorld {
+    /** description for id */
+    id: number;
+    /** description for description */
+    description: string;
+}
